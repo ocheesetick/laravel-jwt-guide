2d33416787ac4a6f29195334da5d8819ca8bc7d5
JT Rael <raeljohntimothy@gmail.com>
2024-03-02 00:11:45 +0800

    Initial push and jwt installation

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // Register (POST, formdata)
+    public function register() {
+
+    }
+
+    // Login (POST, formdata)
+    public function login() {
+
+    }
+
+    // Profile (GET)
+    public function profile() {
+
+    }
+
+    // Refresh Token API (GET)
+    public function refreshToken() {
+
+    }
+
+    // Logout (GET)
+    public function logout() {
+
+    }
+}
