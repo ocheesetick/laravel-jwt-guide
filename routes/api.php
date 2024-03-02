a697106e2f782ce95a142a5302af1e4031bc299b
JT Rael <raeljohntimothy@gmail.com>
2024-03-02 10:18:52 +0800

    Created routes for all the functions

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// API Routes
-Route::post("register", [ApiController::class], "register");
-Route::post("login", [ApiController::class], "login");
+// Public API Routes
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
 
-Route::group([
-    "middleware" => ["auth:api"]
-], function() {
-    Route::get();
+// Protected API Routes
+Route::group(["middleware" => ["auth:api"]], function() {
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("refresh", [ApiController::class, "refreshToken"]);
+    Route::get("logout", [ApiController::class, "logout"]);
 });
